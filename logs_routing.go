57c777a9cbcabf852c6996b759e38d25415efbd9
卜部 昌平 <s-urabe@sakura.ad.jp>
2025-09-02 11:56:51 +0900

    wrap error using NewAPIError
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

--- logs_routing.go
+++ logs_routing.go
@@ -58,7 +58,7 @@ func convertLogRouting(res v1.WrappedLogRouting) v1.LogRouting {
 func (op *logRoutingOp) List(ctx context.Context, params v1.LogsRoutingsListParams) ([]v1.LogRouting, error) {
 	resp, err := op.client.LogsRoutingsList(ctx, params)
 	if err != nil {
-		return nil, err
+		return nil, NewAPIError("LogRouting.List", 0, err)
 	}
 	return resp.Results, nil
 }
