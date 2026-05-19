7b057132f87883aaa2b7a36614519d8799884c50
卜部 昌平 <s-urabe@sakura.ad.jp>
2026-05-19 17:14:42 +0900

    [tools] master branch is abandoned
    see also: https://github.com/golangci/golangci-lint/discussions/6537#discussioncomment-16780523
    
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

--- api/secretmanager/includes/go/common.mk
+++ api/secretmanager/includes/go/common.mk
@@ -43,7 +43,7 @@ dev-tools:
 	$(GO) install github.com/google/go-licenses@v1.0.0
 	$(GO) install github.com/rhysd/actionlint/cmd/actionlint@latest
 	$(GO) install golang.org/x/vuln/cmd/govulncheck@latest
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(go env GOPATH)/bin $(GOLANG_CI_LINT_VERSION)
+	curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $$(go env GOPATH)/bin $(GOLANG_CI_LINT_VERSION)
 
 .PHONY: goimports
 goimports: fmt
